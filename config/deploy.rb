2279008c87805190696776735962f616309f2132
Leo Carten <lcarten14@gmail.com>
2023-11-20 15:52:02 -0500

    new logic

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, 'main'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-set :linked_files, %w{config/database.yml config/secrets.yml db/production.sqlite3}
+set :linked_files, %w{db/production.sqlite3}
 
 # set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
 
