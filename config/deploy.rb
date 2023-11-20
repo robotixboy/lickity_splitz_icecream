8b322c48b67f4ea1fcc86e845accb625e019f81c
Leo Carten <lcarten14@gmail.com>
2023-11-20 15:38:41 -0500

    New DB test

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :branch, 'main'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3', 'db/production.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('db/', 'db/')
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads','db'
 
