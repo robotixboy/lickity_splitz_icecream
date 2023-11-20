bf086ed4c290f3468c1e03599993863a962e726d
Leo Carten <lcarten14@gmail.com>
2023-11-20 15:00:59 -0500

    Rearrangment of DBs

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,9 @@ set :branch, 'main'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-# set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3', 'db/production.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3', 'db/production.sqlite3')
 
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads','db'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads','db'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
